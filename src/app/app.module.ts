3fe976ddba273f4b4c408a131b369aab03967390
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2018-05-14 01:47:51 +0200

    Basic login form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    [BrowserAnimationsModule],
+    [MatInputModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule],
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
