8e95586288b1fb6c50f8916b45f92b6518d31966
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2018-05-26 18:31:26 +0200

    Remove and confirm snack-bars.

---
+++ src/app/interface/shared/snacks/confirm-snack.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'confirm-snack',
+  templateUrl: 'confirm-snack.html',
+  styles: [`.confirm { color: white;, text-align: center; }`],
+})
+export class ConfirmSnack {}
---
+++ src/app/interface/shared/snacks/remove-snack.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'remove-snack',
+  templateUrl: 'remove-snack.html',
+  styles: [`.remove { color: white;, text-align: center; }`],
+})
+export class RemoveSnack {}
